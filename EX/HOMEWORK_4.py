baeb4c2dcc59e31e0b639289818e49b49d18053e
ShayRoz3 <89034761+ShayRoz3@users.noreply.github.com>
2022-07-25 18:27:48 +0300

    Update HOMEWORK_4.py

--- EX/HOMEWORK_4.py
+++ EX/HOMEWORK_4.py
@@ -11,3 +11,19 @@ import urllib.request
 image_url =  "https://she-codes.org/wp-content/uploads/2018/11/cropped-unnamed-1-1.png/"
 urllib.request.urlretrieve(url=image_url, filename='266.png')
 
+
+# participate 
+import urllib.request
+import random
+def picture(url):
+    number = random.randint(0, 1000)
+    filename1 = str(number) + ".jpg"
+    urllib.request.urlretrieve(url, filename=filename1)
+import urllib.request
+import random
+def picture(url):
+    number = random.randint(0, 1000)
+    filename1 = str(number) + ".jpg"
+    urllib.request.urlretrieve(url, filename=filename1)
+
+picture("https://she-codes.org/wp-content/uploads/2018/11/cropped-unnamed-1-1.png")
